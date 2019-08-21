9576568b746d852c968137bf7d29352c752ac608
amruthabathula30 <54269250+amruthabathula30@users.noreply.github.com>
2019-08-21 11:07:04 +0530

    Update MyRunner.java

--- src/test/java/mystepdefinitons/MyRunner.java
+++ src/test/java/mystepdefinitons/MyRunner.java
@@ -11,7 +11,7 @@ import cucumber.api.junit.Cucumber;
 //import cucumber.api.junit.Cucumber;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(features="C:\\Users\\training_h2a.06.15\\Desktop\\Selenium Workspace\\Cucum\\myfeatures\\tags.feature",glue="mystepdef2",tags= {"@Regression"},plugin={"json:target/cucumber.json"})
+@CucumberOptions(features="C:\\Users\\training_h2a.06.15\\Desktop\\Selenium Workspace\\Cucum\\myfeatures\\demotoolslogin.feature",glue="mystepdef2",plugin={"json:target/cucumber.json"})
 
 public class MyRunner {
 
